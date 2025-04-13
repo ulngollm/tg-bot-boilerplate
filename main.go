a53320c56342cce42170f5519600a57ccde579f9
ulngollm <ulngollm@gmail.com>
2025-04-13 23:17:35 +0300

    refactor

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -9,34 +10,42 @@ import (
 	tele "gopkg.in/telebot.v4"
 )
 
-func init() {
+func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("godotenv.Load: %s", err)
 		return
 	}
-}
 
-func main() {
 	t, ok := os.LookupEnv("BOT_TOKEN")
 	if !ok {
 		log.Fatalf("bot token is empty")
 		return
 	}
 
+	log.Println("bot started")
+
+	if err := run(t); err != nil {
+		log.Printf("run: %s", err)
+	}
+
+	log.Println("bot stopped")
+}
+
+func run(token string) error {
 	pref := tele.Settings{
-		Token:  t,
+		Token:  token,
 		Poller: &tele.LongPoller{Timeout: time.Second},
 	}
 	bot, err := tele.NewBot(pref)
 	if err != nil {
-		log.Fatalf("tele.NewBot: %s", err)
-		return
+		return fmt.Errorf("tele.NewBot: %w", err)
 	}
 
 	bot.Handle("/start", handle)
 
 	bot.Start()
+	return nil
 }
 
 func handle(c tele.Context) error {
